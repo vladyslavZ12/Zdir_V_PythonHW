b44e508f9409563d1faec89db145cf347c0295a8
Zdir <v_zdir@ats-hr420-dm12.fc.ca>
2019-10-29 13:34:19 -0400

    made a game

--- game.py
+++ game.py
@@ -11,18 +11,39 @@ computer=choices[randint(0, 2)]
 player = False
 
 while player is False:
-#se player to true
-    player = input("choose rock, paper or scissors\n")
+    #se player to true
+    print("*********************************\n\n")
+    print("choose your weapon!\n\n")
+    print("*********************************\n\n")
+    
+    player = input("choose rock, paper or scissors\n\n")
 
     print("computer chose ", computer, "\n")
     print("player choose ", player, "\n")
 
     if computer == player:
 	    print ("tie! no one wins, play again")
-    elif player =="quit":
+    elif player.lower() =="quit":
         exit()
+    elif player.lower() == "rock":
+        if computer == "paper":
+            print("you lose!", computer, "covers", player,"\n")
+        else:
+        	print("you win!", player, "smashes through", computer, "\n")
+    elif player.lower() == "paper":
+        if computer == "scissors":
+            print("you lose!", computer, "cuts", player,"\n")
+        else:
+        	print("you win!", player, "coveres", computer, "\n")
+    elif player.lower() == "scissors":
+        if computer == "rock":
+            print("you lose!", computer, "smashes", player,"\n")
+        else:
+        	print("you win!", player, "cuts", computer, "\n")
+    else:
+    	print("that's not a valid choice, try again")
 
-# need to check all of our conditions after checking for a tie
+    # need to check all of our conditions after checking for a tie
 
     player = False
     computer = choices[randint(0, 2)]
